a80b0d57b03ca6ea73b97af20f8905f2c78e38e8
Zqyyk11 <zqycharlie.zhang@mail.utoronto.ca>
2025-01-23 10:11:01 +0000

    Updated Calculator and Tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,9 @@ import type { Config } from "tailwindcss";
 
 export default {
   darkMode: ["class"],
-  content: ["./pages/**/*.{ts,tsx}", "./components/**/*.{ts,tsx}", "./app/**/*.{ts,tsx}", "./src/**/*.{ts,tsx}"],
+  content: ["./pages/**/*.{ts,tsx}", "./components/**/*.{ts,tsx}", "./app/**/*.{ts,tsx}", "./src/**/*.{ts,tsx,js,jsx}",
+    './public/index.html',
+  ],
   prefix: "",
   theme: {
     container: {
